f49e15c1958d9e81a09c77c9834194434e73e028
ERichard007 <ecrb29@umsystem.edu>
2025-04-29 12:28:30 -0500

    updated first one

--- Honors/CreativeAccounting.py
+++ Honors/CreativeAccounting.py
@@ -13,3 +13,20 @@ for i in range(days):
 
 print(prefixSum)
 print(dailyProfits)
+
+
+minProfitSegs, maxProfitSegs = float("inf"), float("-inf")
+for segSize in range(minSegSize, maxSegSize):
+    for offset in range(0, segSize-1):
+        start = offset
+        profitCount = 0
+        while (start < days-1):
+            end = min(days-1, (start+segSize)-1)
+            segSum = prefixSum[end] - prefixSum[start]
+            if (segSum > 0):
+                profitCount += 1
+            start += segSize
+        minProfitSegs = min(minProfitSegs, profitCount)
+        maxProfitSegs = max(maxProfitSegs, profitCount)
+
+print(f"{minProfitSegs} {maxProfitSegs}")
