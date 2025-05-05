c41ef79f7b5eab4ca8260df67237ebd9cbbeb787
ERichard007 <ecrb29@umsystem.edu>
2025-05-05 14:35:52 -0500

    streets ahead completed!

--- Honors/StreetsAhead.py
+++ Honors/StreetsAhead.py
@@ -1,1 +1,9 @@
-#start
+streets, drivers = map(int, input().split())
+streetNames = [input() for x in range(streets)]
+streetIndices = {name: i for i,name in enumerate(streetNames)}
+driverDirections = [input().split() for i in range(drivers)]
+
+for driver in driverDirections:
+    start = driver[0]
+    end = driver[1]
+    print(abs(streetIndices.get(end) - streetIndices.get(start))-1)
