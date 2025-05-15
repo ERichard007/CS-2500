f57f8d64c6f6617204cec314d3370ebd58e92faf
ERichard007 <ecrb29@umsystem.edu>
2025-05-15 01:35:55 -0500

    had to try and get answer from online for BranchManager, I got it down but I couldn't for the life of me get past the 4 second time limit. I will look at it more tomorrow to understand the answer.
    Love -- Evan Richard

--- Honors/BranchManager.py
+++ Honors/BranchManager.py
@@ -1,59 +1,45 @@
-def GetParents(city):
-    for child in adjacencyList[city]:
-        parent[child] = city
-        GetParents(child)
+import sys
+sys.setrecursionlimit(2 * 10**5)
+input = sys.stdin.readline
 
-def GetPath(dest):
-    path = []
-    while dest != 0:
-        path.append(dest)
-        dest = parent[dest]
-    return path[::-1]
+num_cities, num_people = map(int, input().split())
 
+adjacency_list = [[] for _ in range(num_cities)]
+parent = [0] * num_cities
 
-numCities, numPeople = map(int, input().split())
+for _ in range(num_cities - 1):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    parent[b] = a
+    adjacency_list[a].append(b)
 
-adjacencyList = [[] for x in range(numCities+1)]
-for x in range(numCities-1):
-    root, child = map(int, input().split())
-    adjacencyList[root].append(child)
+for children in adjacency_list:
+    children.sort()
 
-for child in adjacencyList:
-    child.sort()
+entry_time = [-1] * num_cities
+exit_time = [-1] * num_cities
+time = 0
 
-personDestination = [int(input()) for x in range(numPeople)]
+def dfs(node):
+    global time
+    entry_time[node] = time
+    time += 1
+    for child in adjacency_list[node]:
+        dfs(child)
+    exit_time[node] = time
 
-parent = [0] * (numCities + 1)
-GetParents(1) 
+dfs(0)
 
-ptr = [0] * (numCities + 1)
-peopleArrived = 0
+max_entry_seen = -1
 
-#print("***STARTING***")
-for destination in personDestination:
-    path = GetPath(destination)
-    #print(destination)
-    #print(path)
-    #print(adjacencyList)
+for i in range(num_people):
+    destination = int(input()) - 1
 
-    found = True
-    for node in range(len(path)-1):
-        curr = path[node]
-        next = path[node+1]
-        #print(f'for node {path[node]}')
-        while ptr[curr] < len(adjacencyList[curr]) and adjacencyList[curr][ptr[curr]] < next:
-            ptr[curr] += 1
+    if exit_time[destination] <= max_entry_seen:
+        print(i)
+        exit(0)
 
-        #print(adjacencyList)
+    max_entry_seen = max(max_entry_seen, entry_time[destination])
 
-        if ptr[curr] == len(adjacencyList[curr]) or adjacencyList[curr][ptr[curr]] != next:
-            found = False
-            break
-
-    #print(found)
-    if found: 
-        peopleArrived += 1
-    else:
-        break
-
-print(peopleArrived)
+print(num_people)
